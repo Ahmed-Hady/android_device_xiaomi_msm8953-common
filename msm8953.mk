52490cb0af3e7c706b0bf44f5d6ed281639ecb53
Ahmed Hady <ahmedhady6@gmail.com>
2018-11-22 00:29:44 +0200

    preparing for lineageOS

--- msm8953.mk
+++ msm8953.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
